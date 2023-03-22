bbe80c8f3669cb6ac0566715e0d3ea0f0fe79112
anassohail99 <anas.sohailabs1@gmail.com>
2023-03-22 09:26:27 +0500

    local memory cache

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Inject,
+  CACHE_MANAGER,
+  Injectable,
+} from '@nestjs/common';
+
+import { Cache, caching } from 'cache-manager';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache) {}
+
+  async getHello() {
+    await this.cacheManager.set('cached_item', { key: 32 }, { ttl: 20 });
+    await this.cacheManager.del('cached_item');
+    await this.cacheManager.reset();
+    const cachedItem = await this.cacheManager.get('cached_item');
+    console.log(cachedItem);
     return 'Hello World!';
   }
 }
