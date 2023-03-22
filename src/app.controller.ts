56611135b830ac52572677737d557d79a8524751
anassohail99 <anas.sohailabs1@gmail.com>
2023-03-22 09:27:56 +0500

    overriding global  cache interceptor

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// @UseInterceptors(CacheInterceptor) -> use for only in controller wise
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('manual')
+  async getNumber(): Promise<any> {
     return this.appService.getHello();
   }
 }
