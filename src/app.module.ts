b94683f7c62918f8b46501758dd5f4691604468b
anassohail99 <anas.sohailabs1@gmail.com>
2023-03-22 09:27:08 +0500

    configuration for redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule, CacheInterceptor } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { Socket } from 'dgram';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const redisStore = require('cache-manager-redis-store');
+
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
